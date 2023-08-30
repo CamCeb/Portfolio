0b7add625ef9b2aec37dccd6442b97d156141fd0
Cameron Cebula <camred9@gmail.com>
2023-08-29 20:04:28 -0400

    bug fixes and spell check

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -3,16 +3,16 @@ import aboutme from './images/aboutme.jpg'
 export function AboutMe(){
     return( 
     <>
-        <div className="about-box">
+        <div id="about-box" className="about-box">
             <div className="about-left">
             <img src={aboutme} alt="Aboutme" height="300"/>
             </div>
             <div className="about-right">
                 <h1>About Me:</h1>
-                <p>Welcome to my portfolio website! I am currently a computer engineering student minoring 
-                in engineering management. I am looking for a internship or co-op in software engineering.
-                Through my internship or co-op I hope to learn nessasary skills in the field and find my favorite
-                sector in software. In my free time I enjoy hiking, working out, gaming and completing side projects.
+                <p> Welcome to my portfolio website! I am currently a computer engineering student minoring in
+                    in engineering management. I am currently looking for an internship or co-op in software engineering.
+                    Through my internship or co-op, I hope to learn the necessary skills in the field and find my favorite
+                    sector in software. In my free time, I enjoy hiking, working out, gaming, and completing side projects.
                 </p>
             </div>
             
