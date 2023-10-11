e8ea7ac0ca026af7312a062c6c17896d80a9eda7
Cameron Cebula <camred9@gmail.com>
2023-10-11 16:22:05 -0400

    completing projects section

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -4,14 +4,31 @@ export function Projects(){
             <div id="projects-box" className="projects-box">
                 <h1>Projects:</h1>
                 <div className="projects-row">
-                    <div className="card"></div> 
-                    <div className="card"></div>
-                    <div className="card"></div>
-                </div>
-                <div className="projects-row">
-                <div className="card"></div>
-                <div className="card"></div>
-                <div className="card"></div>
+                    <div className="card" id="card1">
+                        <div className="overlay">
+                            <p>Embedded Systems: <br></br> Multithreaded C Project</p>
+                            <a href='https://github.com/Camceb/ece231Lab2'>
+                            <h1>click here for <br></br>more info &raquo;</h1>
+                            </a>
+                        </div>
+                    </div> 
+                    <div className="card" id="card2">
+                        <div className="overlay">
+                            <p>Stock Tracker [React.JS <br></br> with Finnhub API]</p>
+                            <a href='https://camceb.github.io/stockPractice'>
+                            <h1>click here for <br></br>more info &raquo;</h1>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="card" id="card3">
+                        <div className="overlay">
+                            <p>AI Pong [Python <br></br>Pygame/NEAT] </p>
+                            <a href='https://github.com/CamCeb/PongAI'>
+                            <h1>click here for <br></br>more info &raquo;</h1>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
